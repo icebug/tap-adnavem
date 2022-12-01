e5385e5696493f9a0038ab7e059c8230886c05ff
Eliza Nordén <eliza.norden@gmail.com>
2022-12-01 13:36:13 +0100

    Log debug messages

--- tap_adnavem/streams.py
+++ tap_adnavem/streams.py
@@ -10,6 +10,9 @@ from tap_adnavem.client import AdnavemStream
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
+from singer_sdk.helpers._state import get_writeable_state_dict
+import logging
+
 class PurchaseOrderMasterStream(AdnavemStream):
     """Stream to get purchase order master models."""
     name = "purchase_order"
@@ -17,6 +20,8 @@ class PurchaseOrderMasterStream(AdnavemStream):
 
     primary_keys = ["id"]
     replication_key = "date"
+
+    state_partitioning_keys = ["number"]
 
     schema_filepath = SCHEMAS_DIR / "purchase_orders.json"
 
@@ -42,7 +47,12 @@ class PurchaseOrderMasterStream(AdnavemStream):
         self, row: dict, context: Optional[dict]
     ) -> Dict[str, Any]:
         """As needed, append or transform raw data to match expected structure."""
-        row["extraction_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        row["extraction_date"] = datetime.now()
+        row["date"] = datetime.strptime(row["date"], "%Y-%m-%d")
+
+        logging.info(f'DEBUG MASTER post_process, {self.get_starting_timestamp(context)}')
+        logging.info(f'DEBUG MASTER post_process, {context}')
+
         return row
 
 class PurchaseOrderDocumentStream(AdnavemStream):
@@ -68,11 +78,63 @@ class PurchaseOrderDocumentStream(AdnavemStream):
         params: dict = {}
         params["partyId"] = self.config.get("party_id")
         params["number"] = context["number"]
+
         return params
 
     def post_process(
         self, row: dict, context: Optional[dict]
     ) -> Dict[str, Any]:
         """As needed, append or transform raw data to match expected structure."""
-        row["extraction_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        row["extraction_date"] = datetime.now()
+        row["date"] = datetime.strptime(row["date"], "%Y-%m-%d")
+
+        logging.info(f'DEBUG DOCUMENT post_process, {self.get_starting_timestamp(context)}')
+        logging.info(f'DEBUG DOCUMENT post_process, {context}')
+
         return row
+
+    def _get_state_partition_context(self, context):
+        """Override state handling if Stream.state_partitioning_keys is specified.
+        Args:
+            context: Stream partition or context dictionary.
+        Returns:
+            TODO
+        """
+        if context is None:
+            return None
+
+        if self.state_partitioning_keys is None:
+            return context
+
+        #logging.info("GET STATE PARTITION CONTEXT")
+        #logging.info({k: v for k, v in context.items() if k in self.state_partitioning_keys})
+        return {k: v for k, v in context.items() if k in self.state_partitioning_keys}
+
+    def get_context_state(self, context) -> dict:
+        """Return a writable state dict for the given context.
+        Gives a partitioned context state if applicable; else returns stream state.
+        A blank state will be created in none exists.
+        This method is internal to the SDK and should not need to be overridden.
+        Developers may access this property but this is not recommended except in
+        advanced use cases. Instead, developers should access the latest stream
+        replication key values using
+        :meth:`~singer_sdk.Stream.get_starting_timestamp()` for timestamp keys, or
+        :meth:`~singer_sdk.Stream.get_starting_replication_key_value()` for
+        non-timestamp keys.
+        Partition level may be overridden by
+        :attr:`~singer_sdk.Stream.state_partitioning_keys` if set.
+        Args:
+            context: Stream partition or context dictionary.
+        Returns:
+            A partitioned context state if applicable; else returns stream state.
+            A blank state will be created in none exists.
+        """
+        state_partition_context = self._get_state_partition_context(context)
+        logging.info(f"STATE_PARTITION_CONTEXT {state_partition_context}")
+        if state_partition_context:
+            return get_writeable_state_dict(
+                self.tap_state,
+                self.name,
+                state_partition_context=state_partition_context,
+            )
+        return self.stream_state
--- tap_adnavem/tap.py
+++ tap_adnavem/tap.py
@@ -4,6 +4,11 @@ from typing import List
 
 from singer_sdk import Tap, Stream
 from singer_sdk import typing as th  # JSON schema typing helpers
+
+
+import logging
+from singer_sdk.helpers._state import write_stream_state
+
 # TODO: Import your custom stream types here:
 from tap_adnavem.streams import (
     AdnavemStream,
@@ -43,6 +48,35 @@ class TapAdnavem(Tap):
             description="The token to authenticate against the API service",
         )
     ).to_dict()
+
+    def load_state(self, state: dict) -> None:
+        """Merge or initialize stream state with the provided state dictionary input.
+        Override this method to perform validation and backwards-compatibility patches
+        on self.state. If overriding, we recommend first running
+        `super().load_state(state)` to ensure compatibility with the SDK.
+        Args:
+            state: Initialize the tap's state with this value.
+        Raises:
+            ValueError: If the tap's own state is None, meaning it has not been
+                initialized.
+        """
+        if self.state is None:
+            raise ValueError("Cannot write to uninitialized state dictionary.")
+
+        #logging.info(f'ELIZA DBG LOAD STATE {state.get("bookmarks", {}).items()}')
+        for stream_name, stream_state in state.get("bookmarks", {}).items():
+            # key: i
+            # val: {'context': {'number': number}}
+            for key, val in stream_state.items():
+                #logging.info(f'ELIZA DBG LOAD STATE {key, val}')
+#                for partitioned_key, partitioned_val in val.get("context").items():
+#                    logging.info(f'ELIZA DBG LOAD STATE {partitioned_key, partitioned_val}')
+                write_stream_state(
+                    self.state,
+                    stream_name,
+                    key,
+                    val,
+                )
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
